b9b82a82a5dd572da2aecc849280b9e6d8116d3a
sfnhs-edu <reyyyplays@gmail.com>
2026-07-25 07:23:45 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ function validateLogin() {
   const studentId = document.getElementById("studentId").value.trim();
   
   const validStudents = {
-    "02092006": "John Rey Balasta",
-    "06211993": "Michael Molina",
-    "12345678": "Jose Rosal",
-    "09876543": "Ernesto Dela Cruz"
+    "01020304": "Juan Dela Cruz",
+    "05060708": "Maria Makiling",
+    "09080706": "Jose Rosal",
+    "05040302": "Ernesto Dela Cruz"
   };
   
   if (validStudents.hasOwnProperty(studentId)) {
     
-    localStorage.setItem("studentID", studentId);
-    localStorage.setItem("studentName", validStudents[studentId]);
+    sessionStorage.setItem("loggedIn", "true");
+    sessionStorage.setItem("studentID", studentId);
+    sessionStorage.setItem("studentName", validStudents[studentId]);
     
     window.location.href = "loginaccessapproved.html";
     
